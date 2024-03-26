135bba53746ea572029542ec4fe80c43a1197f5f
Reda96R <redarayyad@yahoo.com>
2024-03-26 02:04:16 +0000

    fixed db problem

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 service mariadb start
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS testbase;"
-mariadb -u root -e "CREATE USER IF NOT EXISTS 'testuser'@'%' IDENTIFIED BY 'testpass';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON testbase.* TO 'testuser'@'%';"
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 mariadb -u root -e "FLUSH PRIVILEGES;"
 
 # mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
